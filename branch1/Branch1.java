f77ba2f40d0b1ffa5378d3fce99bd2555228fff4
emikruc <michal.kruczek@gmail.com>
2019-03-22 09:08:44 +0100

    New Branch created

--- branch1/Branch1.java
+++ branch1/Branch1.java
@@ -1,4 +1,17 @@
 package branch1;
 
-public class Branch1 {
+public class Branch1{
+    int a = 100;
+
+    public Branch1(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
 }
