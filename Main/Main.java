4145ef5c3c69a3706b5e36041a76f0c464596f9a
emikruc <michal.kruczek@gmail.com>
2019-03-22 08:54:29 +0100

    Finished first relase

--- Main/Main.java
+++ Main/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("This is main branch of project");
+
+        System.out.println("This is example of first iteration in project");
     }
 }
