7ee0c6c4a0691f002d199da52f9909b729ca18d1
emikruc <michal.kruczek@gmail.com>
2019-03-22 09:34:24 +0100

    Add new method in branch 1

--- branch1/Branch1.java
+++ branch1/Branch1.java
@@ -4,7 +4,7 @@ package branch1;
 public class Branch1{
     int a = 100;
     public String name = "Misha na master chenged a value";
-    
+
     public Branch1(int a) {
         this.a = a;
     }
@@ -17,6 +17,11 @@ public class Branch1{
         this.a = a;
     }
 
-
-
+    @Override
+    public String toString() {
+        return "Branch1{" +
+                "a=" + a +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
