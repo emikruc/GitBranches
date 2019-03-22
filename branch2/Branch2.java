f7ced0ee69234b69b0b9dcf80992ba5e0d261d8a
emikruc <michal.kruczek@gmail.com>
2019-03-22 09:20:40 +0100

    Branch number 2 created

--- branch2/Branch2.java
+++ branch2/Branch2.java
@@ -1,6 +1,28 @@
 package branch2;
 
 public class Branch2{
+        private int a=23;
+        private String name  = "addery";
 
+    public Branch2(int a, String name) {
+        this.a = a;
+        this.name = name;
+    }
 
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
 }
